63459ca46af3a4040043c040b92b909466e596fb
bruce <793041663@qq.com>
2023-06-16 18:33:44 +0800

    set static page generation timeout

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
+    staticPageGenerationTimeout: 300,
     compiler: {
         styledComponents: true,
     },
